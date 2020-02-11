0424ade130077225e04010f083020b27d1a73b47
Luke Smalley <lukedsmalley@gmail.com>
2020-02-11 14:48:03 -0500

    Start putting together workspace tools

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# oo-kernel-hacking
-Linux kernel workspace for term research in GSU's CSC 4320 (Operating Systems)
+<div align="center">
+  <h1>Object-oriented Kernel Hacking</h1>
+  <p>Linux kernel workspace for term research in GSU's CSC 4320 (Operating Systems) course. Yet another attempt at marrying an intermediate runtime that has first-class support for object orientation with the operating system for great good.</p>
+</div>
+
+## Section Header Text
+This kernel workspace is based on [this Gist](https://gist.github.com/chrisdone/02e165a0004be33734ac2334f215380e) by [Chris Done](https://gist.github.com/chrisdone), since I have not done much like this before. I'll work further into the proper way to do kernel work as I get better aquainted with the kernel source.
---
+++ src/treetool/index.js
@@ -0,0 +1,131 @@
+const { readdir, lstat, readFile, writeFile, readJSON } = require('fs-extra')
+const { join } = require('path')
+const { createHash } = require('crypto')
+const { redBright, blueBright, greenBright } = require('chalk')
+
+async function getSHA(path) { 
+  const hash = createHash('sha512')
+  hash.update(await readFile(path))
+  return hash.digest('base64')
+}
+
+async function getTree(path) {
+  const tree = {
+    size: 0,
+    fileCount: 0,
+    directoryCount: 0,
+    files: {},
+    directories: {}
+  }
+  for (const name of await readdir(path)) {
+    const entry = join(path, name)
+    const stat = await lstat(entry)
+    tree.size += stat.size
+    if (stat.isDirectory()) {
+      const child = await getTree(entry)
+      tree.size += child.size
+      tree.fileCount += child.fileCount
+      tree.directoryCount += child.directoryCount + 1
+      tree.directories[name] = child
+    } else {
+      try {
+        const hash = await getSHA(entry)
+        tree.fileCount++
+        tree.files[name] = {
+          size: stat.size,
+          hash
+        }
+      } catch (err) {
+        console.log(`Failed to compute hash for ${join(path, name)} due to ${err}`)
+      }
+    }
+  }
+  return tree
+}
+
+function formatCount(n) {
+  n = String(n)
+  formattedN = ''
+  for (let i = n.length; i--; ) {
+    formattedN = n.charAt(i) + formattedN
+    if ((n.length - i) % 3 === 0 && i > 0) {
+      formattedN = ',' + formattedN
+    }
+  }
+  return formattedN
+}
+
+function formatDifference(n) {
+
+}
+
+function compareTrees(from, to, base='') {
+  for (const name in from.files) {
+    if (!to.files[name]) {
+      console.log(`${redBright('-')}  F  ${join(base, name)}`)
+    } else if (to.files[name].size !== from.files[name].size
+        || to.files[name].hash !== from.files[name].hash) {
+      console.log(`${blueBright('>')}  F  ${join(base, name)}`)
+    }
+  }
+
+  for (const name in to.files) {
+    if (!from.files[name]) {
+      console.log(`${greenBright('+')}  F  ${join(base, name)}`)
+    }
+  }
+
+  for (const name in from.directories) {
+    if (!to.directories[name]) {
+      console.log(`${redBright('-')}  D  ${join(base, name)}`)
+    } else {
+      compareTrees(from.directories[name], to.directories[name], name)
+    }
+  }
+
+  for (const name in to.directories) {
+    if (!from.directories[name]) {
+      console.log(`${greenBright('+')}  D  ${join(base, name)}`)
+    }
+  }
+}
+
+async function main() {
+  if (process.argv.length < 3) {
+    console.log('Expected dtree command ("snapshot" or "compare")')
+    console.log()
+    console.log('Usage: dtree (snapshot|s) [<source-dir>] <snap-file-dest>')
+    console.log('       dtree (compare|c) <from-tree-file> <to-tree-file>')
+  } else if ('snapshot'.startsWith(process.argv[2])) {
+    if (process.argv.length < 4) {
+      console.log('Expected source directory and/or snapshot destination')
+      console.log()
+      console.log('Usage: dtree (snapshot|s) [<source-dir>] <snap-file-dest>')
+    } else {
+      const destination = process.argv[process.argv.length < 5 ? 3 : 4]
+      const source = process.argv.length < 5 ? '.' : process.argv[3]
+      const tree = await getTree(source)
+      await writeFile(destination, JSON.stringify(tree, null, 2), { encoding: 'utf8' })
+      console.log(`Snapshot of ${source} saved into ${destination}`)
+      console.log(`with ${tree.fileCount} files and ${tree.directoryCount} directories found (${formatCount(tree.size)} bytes)`)
+    }
+  } else if ('compare'.startsWith(process.argv[2])) {
+    if (process.argv.length < 5) {
+      console.log('Expected two snapshot file paths')
+      console.log()
+      console.log('Usage: dtree (compare|c) <from-snap-file> <to-snap-file>')
+    } else {
+      compareTrees(
+        await readJSON(process.argv[3], { encoding: 'utf8' }),
+        await readJSON(process.argv[4], { encoding: 'utf8' }))
+    }
+  } else {
+    console.log('Expected dtree command ("snapshot" or "compare")')
+    console.log()
+    console.log('Usage: dtree (snapshot|s) [<source-dir>] <snap-file-dest>')
+    console.log('       dtree (compare|c) <from-tree-file> <to-tree-file>')
+  }
+  console.log()
+}
+
+main().catch(err => console.log(`Unhandled ${err}`))
