dff7f2ca8f46a6890ba951746ebf2c4e4358f47f
almorrison <60953282+almorrison@users.noreply.github.com>
2020-04-08 12:06:15 -0400

    Update ciltool.py

--- src/ciltool/ciltool.py
+++ src/ciltool/ciltool.py
@@ -1,3 +1,4 @@
+# use python3
 import shlex
 import subprocess
 import sys
@@ -7,8 +8,8 @@ command = "dotnet build %s" % sys.argv[1]
 arg = shlex.split(command)
 subprocess.call(arg)
 
-build_path = raw_input("")
-output_path = raw_input("")
+build_path = input("")
+output_path = input("")
 
 command = "dotnet-ildasm %s -f -o %s" % (build_path, output_path)
 arg = shlex.split(command)
